877c24003080941cd81eb495fb9657c16600cf3e
Poul Bach <poulbachlauritsen@gmail.com>
2019-11-06 10:25:48 +0100

    Tilføjet Randers afdeling
    De har billigere cykler.

--- YderligereTræning/YderligereTræning/BikeShop.cs
+++ YderligereTræning/YderligereTræning/BikeShop.cs
@@ -12,21 +12,17 @@ namespace YderligereTræning
         public BikeShop()
         {
             bikeList = new List<Bicycle>();
-            bikeList.Add(new Bicycle("Centurion", 26.5, 3899.95, Color.RED));
-            bikeList.Add(new Bicycle("Centurion", 27.0, 4899.95, Color.BROWN));
-            bikeList.Add(new Bicycle("Centurion", 26.0, 3400.00, Color.GREEN));
-            bikeList.Add(new Bicycle("Look", 28.0, 6999.00, Color.YELLOW));
-            bikeList.Add(new Bicycle("Look", 18.5, 899.95, Color.CYAN));
-            bikeList.Add(new Bicycle("Look", 2109310293.5, 353452435899.95, Color.BLUE));
-            bikeList.Add(new Bicycle("RALEIGH", 27.5, 5999.99, Color.WHITE));
-            bikeList.Add(new Bicycle("RALEIGH", 27.5, 5999.99, Color.BLACK));
-            bikeList.Add(new Bicycle("RALEIGH", 27.5, 5999.99, Color.GREY));
         }
 
 
         public List<Bicycle> GetAllBikes()
         {
             return bikeList;
+        }
+
+        public void AddBike(Bicycle bike)
+        {
+            bikeList.Add(bike);
         }
 
         public List<Bicycle> GetBikesWithLargerWheelSize(double size)
@@ -92,7 +88,6 @@ namespace YderligereTræning
             }
             else
             {
-                Console.WriteLine("Brand not found.");
             }
             return brandList;
         }
--- YderligereTræning/YderligereTræning/Program.cs
+++ YderligereTræning/YderligereTræning/Program.cs
@@ -11,11 +11,33 @@ namespace YderligereTræning
             BikeShop PoulsCykler = new BikeShop();
             BikeShop PoulsCyklerRanders = new BikeShop();
 
+            PoulsCykler.AddBike(new Bicycle("Centurion", 26.5, 3899.95, Color.RED));
+            PoulsCykler.AddBike(new Bicycle("Centurion", 27.0, 4899.95, Color.BROWN));
+            PoulsCykler.AddBike(new Bicycle("Centurion", 26.0, 3400.00, Color.GREEN));
+            PoulsCykler.AddBike(new Bicycle("Look", 28.0, 6999.00, Color.YELLOW));
+            PoulsCykler.AddBike(new Bicycle("Look", 18.5, 899.95, Color.CYAN));
+            PoulsCykler.AddBike(new Bicycle("Look", 2109310293.5, 353452435899.95, Color.BLUE));
+            PoulsCykler.AddBike(new Bicycle("RALEIGH", 27.5, 5999.99, Color.WHITE));
+            PoulsCykler.AddBike(new Bicycle("RALEIGH", 27.5, 5999.99, Color.BLACK));
+            PoulsCykler.AddBike(new Bicycle("RALEIGH", 27.5, 5999.99, Color.GREY));
+
+            PoulsCyklerRanders.AddBike(new Bicycle("Ribble", 26.5, 1899.95, Color.RED));
+            PoulsCyklerRanders.AddBike(new Bicycle("Ribble", 27.0, 1999.95, Color.BROWN));
+            PoulsCyklerRanders.AddBike(new Bicycle("Ribble", 26.0, 2400.00, Color.GREEN));
+            PoulsCyklerRanders.AddBike(new Bicycle("Canyon", 22.0, 999.00, Color.YELLOW));
+            PoulsCyklerRanders.AddBike(new Bicycle("Canyon", 18.5, 699.95, Color.CYAN));
+            PoulsCyklerRanders.AddBike(new Bicycle("Canyon", 42.0, 133.7, Color.BLUE));
+            PoulsCyklerRanders.AddBike(new Bicycle("Radon", 27.5, 3999.99, Color.WHITE));
+            PoulsCyklerRanders.AddBike(new Bicycle("Radon", 27.5, 3999.99, Color.BLACK));
+            PoulsCyklerRanders.AddBike(new Bicycle("Radon", 27.5, 3999.99, Color.GREY));
+
+
             PoulsEmporium.bikeShops.Add(PoulsCykler);
             PoulsEmporium.bikeShops.Add(PoulsCyklerRanders);
 
             Console.WriteLine("All brands available:");
             List<string> brands = PoulsCykler.GetAllBrands();
+            brands.AddRange(PoulsCyklerRanders.GetAllBrands());
 
             foreach (string str in brands)
             {
@@ -23,8 +45,9 @@ namespace YderligereTræning
             }
 
             Console.WriteLine("\n\n\nSkriv et mærke du vil finde cykler fra.");
-
-            PoulsCykler.GetAllBicyclesOfBrand(Console.ReadLine());
+            string input = Console.ReadLine();
+            PoulsCykler.GetAllBicyclesOfBrand(input);
+            PoulsCyklerRanders.GetAllBicyclesOfBrand(input);
 
             Console.WriteLine("\n\n\nRed bikes amount");
             PoulsEmporium.ReturnBikeAmountFromColor(Color.RED);
