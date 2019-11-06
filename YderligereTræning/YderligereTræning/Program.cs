1a9d680d5629c2b7628d4c90dc1dac16c9f541a2
Poul Bach <poulbachlauritsen@gmail.com>
2019-11-06 08:40:13 +0100

    Yderligere træning first commit

---
+++ YderligereTræning/YderligereTræning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YderligereTræning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
