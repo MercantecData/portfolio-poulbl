2063ffd817ab473ba5a7d14d1db054664c2e8b75
Poul Bach <poulbachlauritsen@gmail.com>
2019-11-11 13:37:54 +0100

    MEGET vigtig knap!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    æsel

--- docs/sjov.js
+++ docs/sjov.js
@@ -1,7 +1,43 @@
 
-console.log("hello world");
 
 function testKlik()
 {
     document.getElementById("testKnap").innerHTML = "Paragraph changed.";
+}
+
+
+function funkTion()
+{
+  let rand = Math.random();
+  if(rand > 0.9)
+  {
+    document.getElementById("body").style.background = 'red';
+  }
+  else if (rand > 0.8) {
+    document.getElementById("body").style.background = 'blue';
+  }
+  else if (rand > 0.7) {
+    document.getElementById("body").style.background = 'pink';
+  }
+  else if (rand > 0.6) {
+    document.getElementById("body").style.background = 'cyan';
+  }
+  else if (rand > 0.5) {
+    document.getElementById("body").style.background = 'yellow';
+  }
+  else if (rand > 0.4) {
+    document.getElementById("body").style.background = 'green';
+  }
+  else if (rand > 0.3) {
+    document.getElementById("body").style.background = 'orange';
+  }
+  else if (rand > 0.2) {
+    document.getElementById("body").style.background = 'purple';
+  }
+  else if (rand > 0.1) {
+    document.getElementById("body").style.background = '#6B6A6A';
+  }
+  else if (rand > 0.0) {
+    document.getElementById("body").style.background = '#FFFFFFF';
+  }
 }
